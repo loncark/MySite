2fa5b29021381e54ff3fa19cb51e9e20bbde3bcd
Kristina Lončar <loncar.kristina1998@gmail.com>
2025-12-29 16:03:14 +0100

    Noice Start

--- src/components/GlassCard.vue
+++ src/components/GlassCard.vue
@@ -1,11 +1,24 @@
 <template>
-      <p>Hello, my name is Kristina.</p>
+  <div class="glass-card">
+    <p>Hello, my name is Kristina.</p>
+  </div>
 </template>
 
-<script>
-
+<script setup>
 </script>
 
 <style scoped>
-    
+.glass-card {
+  background: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  border-radius: 20px;
+  padding: 10px 20px 10px 20px;
+  box-shadow: 0 0 30px rgba(100, 200, 255, 0.3);
+}
+
+p {
+  margin: 0;
+  text-align: center;
+}
 </style>
